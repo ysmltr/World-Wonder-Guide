a1806699cf68cbb0c2c25316d7e5ad294dde2e9e
ysmltr <yesimltr1@gmail.com>
2023-12-17 22:39:50 +0300

    Refactor code for improved performance and readability

---
+++ main.js
@@ -0,0 +1,76 @@
+let searchBtn = document.getElementById("search-btn");
+
+let countryInp = document.getElementById("country-inp");
+
+
+
+
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+  console.log(finalURL);
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        //  console.log(data[0]);
+        //  console.log(data[0].capital[0]);
+        //  console.log(data[0].flags.svg);
+        //  console.log(data[0].name.common);
+        //  console.log(data[0].continents[0]);
+        //  console.log(Object.keys(data[0].currencies)[0]);
+        //  console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        //  console.log(Object.values(data[0].languages).toString().split(",").join(", ")
+         //);
+         result.innerHTML= `
+         <img src="${data[0].flags.svg}"
+         class="flag-img"/>
+         <h2>${data[0].name.common}</h2>
+           <div class="wrapper">
+              <div class="data-wrapper">
+                <h4>Capital:</h4>
+                  <span>${data[0].capital[0]}</span>
+        </div>
+  </div>
+
+    <div class="wrapper">
+              <div class="data-wrapper">
+                <h4>Continents:</h4>
+                  <span>${data[0].continents[0]}</span>
+        </div>
+      </div>    
+      
+      <div class="wrapper">
+        <div class="data-wrapper">
+          <h4>Population:</h4>
+            <span>${data[0].population.toLocaleString()}</span>
+      </div>
+    </div> 
+
+    <div class="wrapper">
+      <div class="data-wrapper">
+        <h4>Currency:</h4>
+          <span>${data[0].currencies[Object.keys(data[0].currencies)].name}
+          - ${Object.keys(data[0].currencies)[0]}</span>
+      </div>
+    </div> 
+
+    <div class="wrapper">
+      <div class="data-wrapper">
+        <h4>Common Languages:</h4>
+          <span>${Object.values(data[0].languages)
+            .toString()
+            .split(",")
+            .join(", ")}
+            </span>
+      </div>
+    </div> 
+         `
+    })
+    .catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `<h3>Whoopsie-daisy! Looks like someone forgot to type a country. 😅</h3>`;
+      } else {
+        result.innerHTML = `<h3>*No cap, that ain't a real country. Enter something legit, not like Area 51 or Wakanda. 🌌🔍</h3>`;
+      }
+    });
+});
